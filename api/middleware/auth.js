517db1ad721abcd2cefa55acb15ec783326c2001
John ayinde <lolaayinde@gmail.com>
2020-09-24 14:50:50 +0100

    middlewares

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -6,22 +6,20 @@ const auth = async (req, res, next) => {
   try {
 
     const token = await req.header('x-auth-token');
-    debug("token", token)
+    // debug("token", token)
     if (!token) return res.status(401).send("Access denied. No token provided");
 
-
     const data = jwt.verify(token, process.env.JWT_KEY);
-    debug("data", data)
 
 
     const user = await User.findById({ _id: data._id });
-    debug("user", user)
 
     if (!user) {
       return res.status(401).send("invalid token");
 
     } else {
       req.user = data;
+      // debug("user", req.user);
       next();
     }
 
