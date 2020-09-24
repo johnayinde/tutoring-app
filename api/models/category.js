dc1cdf2a92925ddd7df664b9de8cecbe590f43cf
John ayinde <lolaayinde@gmail.com>
2020-09-24 14:48:03 +0100

    category routes, and logics

--- api/models/category.js
+++ api/models/category.js
@@ -2,29 +2,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const categorySchema = new Schema({
-    name: {
-        emum: ['student', 'tutor'],
-        type: String
-    },
-    subjects: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Subject'
-    }],
-    tutors: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Tutor'
-    }],
-    students: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    lesson: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Lesson'
-    }],
-}, { timestamps: true });
+  category: {
+    enum: ["student", "tutor"],
+    type: String,
+    required: true,
+  },
+  subjects: [{
+    type: Schema.Types.ObjectId,
+    ref: "Subject",
+  }],
+  tutors: [{
+    type: Schema.Types.ObjectId,
+    ref: "Tutor",
+  }],
+  students: [{
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  }],
+  lessons: [{
+    type: Schema.Types.ObjectId,
+    ref: "Lesson",
+  }],
+},
+  { timestamps: true }
+);
 
-
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model("Category", categorySchema);
